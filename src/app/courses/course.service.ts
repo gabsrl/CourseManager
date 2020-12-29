54d5a6f1206d6892e2137616c302c59c6c5ad626
Gabriel Lima <grl@icomp.ufam.edu.br>
2020-12-28 21:48:23 -0400

    primeiro commit

---
+++ src/app/courses/course.service.ts
@@ -0,0 +1,79 @@
+import { Course } from './course';
+import { Injectable } from '@angular/core';
+
+
+/**
+ * Estamos tornando esta classe acessível para toda a aplicação angular.
+ * Isso é injeção de dependência. Esse objeto estará acessível em qualquer lugar.
+ * É possível escopar a injenção de depência para módulos.
+ * Classes de serviço não devem alterar variáveis.
+ * Classes de serviço devem chamar/dar resultado de outras operações.
+ */
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CourseService {
+
+  retrieveAll(): Course[] {
+    return COURSES;
+  }
+}
+
+var COURSES: Course[] = [
+  {
+      id: 1,
+      name: 'Angular: CLI',
+      releaseDate: 'November 2, 2019',
+      description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+      duration: 120,
+      code: 'XLF-1212',
+      rating: 3,
+      price: 12.99,
+      imageUrl: '/assets/images/cli.png',
+  },
+  {
+      id: 2,
+      name: 'Angular: Forms',
+      releaseDate: 'November 4, 2019',
+      description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
+      duration: 80,
+      code: 'DWQ-3412',
+      rating: 3.5,
+      price: 24.99,
+      imageUrl: '/assets/images/forms.png',
+  },
+  {
+      id: 3,
+      name: 'Angular: HTTP',
+      releaseDate: 'November 8, 2019',
+      description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
+      duration: 80,
+      code: 'QPL-0913',
+      rating: 4.0,
+      price: 36.99,
+      imageUrl: '/assets/images/http.png',
+  },
+  {
+      id: 4,
+      name: 'Angular: Router',
+      releaseDate: 'November 16, 2019',
+      description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
+      duration: 80,
+      code: 'OHP-1095',
+      rating: 4.5,
+      price: 46.99,
+      imageUrl: '/assets/images/router.png',
+  },
+  {
+      id: 5,
+      name: 'Angular: Animations',
+      releaseDate: 'November 25, 2019',
+      description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
+      duration: 80,
+      code: 'PWY-9381',
+      rating: 5,
+      price: 56.99,
+      imageUrl: '/assets/images/animations.png',
+  }
+];
---
+++ src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course {
+  id: number;
+  imageUrl: string;
+  name: string;
+  description: string;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+}
